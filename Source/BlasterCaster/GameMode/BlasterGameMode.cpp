7726ab541151a5e8b346bd9af1bec57b2e550cb3
Mehmet Tanis <55228969+Melon332@users.noreply.github.com>
2022-06-21 18:26:42 +0200

    Added a cool material and fixed bug with spawning

--- Source/BlasterCaster/GameMode/BlasterGameMode.cpp
+++ Source/BlasterCaster/GameMode/BlasterGameMode.cpp
@@ -25,9 +25,44 @@ void ABlasterGameMode::RequestRespawn(ACharacter* EliminatedPlayer, AController*
 	}
 	if(EliminatedController)
 	{
-		TArray<AActor*> PlayerStarts;
-		UGameplayStatics::GetAllActorsOfClass(this, APlayerStart::StaticClass(),PlayerStarts);
-		int32 Selection = FMath::RandRange(0, PlayerStarts.Num() - 1);
-		RestartPlayerAtPlayerStart(EliminatedController, PlayerStarts[Selection]);
+		RestartPlayerAtPlayerStart(EliminatedController, FindRandomSpawnPosition(EliminatedPlayer));
 	}
+}
+
+AActor* ABlasterGameMode::FindRandomSpawnPosition(AActor* EliminatedPlayer)
+{
+	TArray<AActor*> PlayerStarts;
+	UGameplayStatics::GetAllActorsOfClass(this, APlayerStart::StaticClass(),PlayerStarts);
+	int32 Selection = FMath::RandRange(0, PlayerStarts.Num() - 1);
+	/*
+	TArray<AActor*> Players;
+	UGameplayStatics::GetAllActorsOfClass(this, ABlasterCharacter::StaticClass(), Players);
+	float FurthestDist{0};
+	AActor* FurthestStartPos{nullptr};
+	for(AActor* StartPos : PlayerStarts)
+	{
+		for(AActor* Player : Players)
+		{
+			if(Player == EliminatedPlayer) continue;
+			float Dist = Player->GetDistanceTo(StartPos);
+			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Blue, FString::Printf(TEXT("Distance is: %f"), Dist));
+			if(Dist >= DistanceBetweenPlayers)
+			{
+				FurthestDist += Dist;
+				if(FurthestDist >= Dist)
+				{
+					FurthestStartPos = StartPos;
+					FurthestDist = Dist;
+				}
+			}
+		}
+	}
+	
+	
+	if(!FurthestStartPos)
+	{
+		return PlayerStarts[Selection];
+	}
+	*/
+	return PlayerStarts[Selection];
 }
--- Source/BlasterCaster/GameMode/BlasterGameMode.h
+++ Source/BlasterCaster/GameMode/BlasterGameMode.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#define DISTANCE_BETWEEN_PLAYER 500.f;
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
 #include "BlasterGameMode.generated.h"
@@ -16,4 +17,10 @@ class BLASTERCASTER_API ABlasterGameMode : public AGameMode
 public:
 	virtual void PlayerEliminated(class ABlasterCharacter* EliminatedPlayer, class ABlasterPlayerController* EliminatedPlayerController, ABlasterPlayerController* AttackerController);
 	virtual void RequestRespawn(ACharacter* EliminatedPlayer, AController* EliminatedController);
+protected:
+private:
+	AActor* FindRandomSpawnPosition(AActor* EliminatedPlayer);
+
+	UPROPERTY(EditDefaultsOnly)
+	float DistanceBetweenPlayers{200.f};
 };
